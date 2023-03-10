ed5ad08056167da1949b72a7ecc1bafd92bf7566
aurelioklv <aurelioklv@gmail.com>
2023-03-10 17:43:54 +0700

    feat: add input validation

--- login.js
+++ login.js
@@ -1,6 +1,13 @@
 const form = document.querySelector("form");
+const email_val = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
 form.onsubmit = () => {
+    var email = document.getElementById("email").value;
+
+    if (!email_val.test(email)) {
+        alert("Invalid email format. Please enter valid email");
+        return;
+    }
     alert("Login successfully");
 };
 
--- signup.js
+++ signup.js
@@ -2,3 +2,32 @@ const form = document.querySelector("form");
 
 const link_login = document.querySelector(".form .link .login-link");
 link_login.href = "index.html";
+
+function create_account() {
+    var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var confirmationPassword = document.getElementById("confirmation-password").value;
+
+    var letters = /^[A-Za-z]+$/;
+    var email_val = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+    if (username == '' || email == '' || password == '' || confirmationPassword == '') {
+        alert("Enter each details correctly");
+    }
+    else if (!letters.test(username)) {
+        alert("Name is incorrect. Must contain alphabets");
+    }
+    else if (!email_val.test(email)) {
+        alert("Invalid email format. Please enter valid email");
+    }
+    else if (password != confirmationPassword) {
+        alert("Password not matching");
+    }
+    else if (document.getElementById("password").value.length > 12) {
+        alert("Password maximum length is 12");
+    }
+    else {
+        alert("Your account has been created successfully");
+    }
+}
